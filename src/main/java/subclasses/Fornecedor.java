dfe12c35eecdaccfe241d2f4380883094da265df
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-08-09 18:28:27 -0300

    Classe Pessoa e Fornecedor testado

---
+++ src/main/java/subclasses/Fornecedor.java
@@ -0,0 +1,38 @@
+package main.java.subclasses;
+
+import main.java.superclasse.Pessoa;
+
+public class Fornecedor extends Pessoa {
+
+    private Double valorCredito;
+    private Double valorDivida;
+
+    public Fornecedor() {
+    }
+
+    public Fornecedor(String nome, String endereco, String telefone, Double valorCredito, Double valorDivida) {
+        super(nome, endereco, telefone);
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    public Double getValorCredito() {
+        return valorCredito;
+    }
+
+    public void setValorCredito(Double valorCredito) {
+        this.valorCredito = valorCredito;
+    }
+
+    public Double getValorDivida() {
+        return valorDivida;
+    }
+
+    public void setValorDivida(Double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
+
+    public Double obterSaldo() {
+        return valorCredito - valorDivida;
+    }
+}
---
+++ src/main/java/test/FornecedorTest.java
@@ -0,0 +1,18 @@
+package main.java.test;
+
+import main.java.subclasses.Fornecedor;
+
+public class FornecedorTest {
+    public static void main(String[] args) {
+
+        Fornecedor fornecedor = new Fornecedor("Leonardo", "QR 403 Conjunto 11 Casa 13",
+                "61995568949", 69000.0, 58000.0);
+        double saldo = fornecedor.obterSaldo();
+        System.out.println("Nome: " + fornecedor.getNome());
+        System.out.println("Endereço: " + fornecedor.getEndereco());
+        System.out.println("Telefone: " + fornecedor.getTelefone());
+        System.out.println("Valor de crédito: R$" + String.format("%.2f", fornecedor.getValorCredito()));
+        System.out.println("Valor da dívida: R$" + String.format("%.2f", fornecedor.getValorDivida()));
+        System.out.println("Saldo: R$" + String.format("%.2f", saldo));
+    }
+}
